48dd88b63e0ec41ca92657b1ddaf4873f6dea23f
SkvOl <77995388+SkvOl@users.noreply.github.com>
2022-08-25 19:28:17 +0300

    Update maybe_diplom.cpp

--- maybe_diplom/maybe_diplom.cpp
+++ maybe_diplom/maybe_diplom.cpp
@@ -1,28 +1,25 @@
-﻿#include "matrix.h"
+#include "matrix.h"
 #include "integrals.h"
 #include <stdio.h> 
-#include <ctime> 
+#include <time.h> 
 
 const double pi = 3.1415926, Eps = 0.0001;
 const double  k0 = 1, k1 = 1.5 * k0;
 
-int n = 2, N = n * n;
+const int n = 5, N = n * n;
 
 double R = 5;
-double lambda = -1;
+double lambda = 1;
 
-//область для интегральных уравнений
+//отрезок для двумерных интегральных уравнений
 double A = 0, B = 1;
 double C = 0, D = 1;
-double E = 0, F = 1;
 
 //шаг для одномерных интегральных уравнений
 double h = (B - A) / n;
 
-//шаг многомерных интегральных уравнений
-double h1 = (B - A) / n, 
-       h2 = (D - C) / n,
-       h3 = (F - E) / n;
+//отрезок для двумерных интегральных уравнений
+double h1 = (B - A) / n, h2 = (D - C) / n;
 
 
 inline double u(double y1) {     
@@ -68,8 +65,9 @@ void mg(double**& var, size_t type, size_t dim_s = 1) {
     //метод Галёркина
     //var - матрица, в которую будут записываться данные 
     //type - тип уравнения Фредгольма, первого или второго рода
-    //dim_s - размерность измерения в котором считается метод Галёркина
+    //dim_s - размер измерения в котором считается метод Галёркина
     size_t M = _msize(var) / sizeof(var[0]);
+    int m = (int)sqrt(M);
 
     if (dim_s == 1) {
         for (size_t i = 0; i < M; i++) {
@@ -78,13 +76,12 @@ void mg(double**& var, size_t type, size_t dim_s = 1) {
             for (size_t j = 0; j < M; j++) {
                 double c = A + j * h, d = A + (j + 1.0) * h;
 
-                var[i][j] = h * base_func(i, j) * (type - 1.0) + lambda * I(100, k, a, b, c, d);
+                var[i][j] = h * base_func(i, j) * (type - 1.0) - lambda * I(100, k, a, b, c, d);
             }
             var[i][M] = I(100, a, b);
         }
     }
-    else if (dim_s == 2) {
-        int m = (int)sqrt(M);
+    else {
         for (size_t i = 0; i < M; i++) {
             short i1 = i / m, i2 = i % m;
             double a = A + i1 * h1, b = A + (i1 + 1.0) * h1;
@@ -97,34 +94,9 @@ void mg(double**& var, size_t type, size_t dim_s = 1) {
                 double g = C + j2 * h2, l = C + (j2 + 1.0) * h2;
 
                 //printf("j=%d\n", j);
-                var[j][i] = h1 * h2 * base_func(i, j) * (type - 1.0) + lambda * I(10, k, a, b, c, d, e, f, g, l);
+                var[j][i] = h1 * h2 * base_func(i, j) * (type - 1.0) - lambda * I(10, k, a, b, c, d, e, f, g, l);
             }
             var[i][M] = I(100, func, a, b, c, d);
-        }
-    }
-    else if (dim_s == 3) {
-        int m = (int)pow(M, 1.0 / 3.0);
-        
-        for (size_t i = 0; i < M; i++) {
-            int i1 = i % m, i2 = i / m - m * (i / m / m), i3 = i / m / m;
-            
-            double a = A + i1 * h1, b = A + (i1 + 1.0) * h1;
-            double c = C + i2 * h2, d = C + (i2 + 1.0) * h2;
-            double e = E + i3 * h3, f = E + (i3 + 1.0) * h3;
-
-            printf("i=%d\n", i);
-            for (size_t j = 0; j < M; j++) {
-                int j1 = j % m, j2 = j / m - m * (j / m / m), j3 = j / m / m;
-                
-                double g = A + j1 * h1, l = A + (j1 + 1.0) * h1;
-                double o = C + j2 * h2, p = C + (j2 + 1.0) * h2;
-                double q = E + j3 * h3, s = E + (j3 + 1.0) * h3;
-                
-
-                var[i][j] = h1 * h2 * h3 * base_func(i, j) * (type - 1.0) + lambda * I(10, k, a, b, c, d, g, l, o, p, q, s, e, f);
-            }
-
-            var[i][M] = I(10, func, a, b, c, d, e, f);
         }
     }
 }
@@ -132,6 +104,7 @@ void mg(double**& var, size_t type, size_t dim_s = 1) {
 
 int main1()
 {
+    return 0;
     double** a = createm<double>(n, n + 1.0);
 
     //mk(a, 2, 2);
@@ -159,15 +132,11 @@ int main1()
 }
 
 
-int main2() {
-    return 0;
+int main() {
+    //return 0;
     double **a = createm<double>(N, N + 1.0), **a1 = createm<double>(N, N + 1.0);
-    time_t start1, end1, start2, end2;
 
-
-    time(&start1);
     mg(a, 2.0, 2);
-    time(&end1);
     
     mk(a1, 2.0, 2);
 
@@ -179,9 +148,9 @@ int main2() {
     //print(a1);
     //space(1);
 
-    time(&start2);
+
     gm(a);
-    time(&end2);
+
 
     gm(a1);
 
@@ -201,38 +170,10 @@ int main2() {
     {
         /*short i1 = i / n, i2 = i % n;
         double ksi1 = A + (i1 + 0.5) * h1, ksi2 = A + (i2 + 0.5) * h2;
-
         printf("%f %f %f\n", ksi1, ksi2, a1[i][N]);*/
         
         if (i % n == 0 && i != 0) printf("\n");
         printf("%f ", a1[i][N]);
     }
 
-    double time1 = difftime(end1, start1), time2 = difftime(end2, start2);
-    printf("\nTime is: %f\n", time1 + time2);
-}
-
-int main() {
-    N *= n;
-    double** a = createm<double>(N, N + 1.0);
-    double time1, time2;
-
-    time1 = clock();
-    mg(a, 2.0, 3);
-    time2 = clock();
-    printf("Time Galerkin method is: %f sec.\n", (time2 - time1) / 1000.0);
-    print(a);
-    space(1);
-    time1 = clock();
-    gm(a);
-    time2 = clock();
-    printf("Time Gauss method is: %f sec.\n", (time2 - time1) / 1000.0);
-    print(a);
-    space(1);
-
-    for (size_t i = 0; i < N; i++)
-    {
-        int i1 = i % n, i2 = i / n - n * (i / n / n), i3 = i / n / n;
-        printf("%f %f %f %f\n", A + h1 * i1 / 2.0, C + h2 * i2 / 2.0, E + h3 * i3 / 2.0, a[i][N]);
-    }
 }
